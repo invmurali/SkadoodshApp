a2acf3352a805d604c984563d6bfd7408f30598d
invmurali <murali.uppangala@invenger.com>
2015-06-23 12:38:38 +0530

    Skadoosh Commit

---
+++ SkadooshApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkadooshApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nyra Escaped!! Press Enter to find.......");
+            Console.ReadLine();
+
+        }
+    }
+}
